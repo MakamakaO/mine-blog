13fa9fed23c93f8489ff9b30eeb0e19b85ab12bf
MakamakaO <20221866@stud.tjut.edu.cn>
2024-04-22 16:19:34 +0800

    header faile run

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,14 @@
+const require = createRequire(import.meta.url);
+import { createRequire } from 'module'; // at top of your file
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,ts,jsx,tsx} "
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 }
